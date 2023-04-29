100e3c0cd2eb14ee69562251ecad631592bf259f
Eduarda Graziele de Paiva <114159027+PaivaEduarda@users.noreply.github.com>
2023-04-28 23:21:03 -0300

    Atualização 24/04/23
    Fiz o movimento do monstro e do herói, limitei a borda e iniciou um erro no método resetar

--- Pair programing/Pair programing/Script.js
+++ Pair programing/Pair programing/Script.js
@@ -14,16 +14,29 @@ var yHeroi = 100;
 var xMonstro=200;
 var yMonstro=100;
 
+// Controle de Posicionamento do Heroi
+var xHeroiR = 100;
+var yHeroiR = 100;
+
+// Controle de Posicionamento do Monstro
+var xMonstroR =200;
+var yMonstroR =100;
+
 var imgHeroi = new Image();
 imgHeroi.src = "img/heroi.png";
 var imgMonstro = new Image();
 imgMonstro.src = "img/monstro.png";
 
+function Resetar()
+{
+    objContexto.drawImage(imgFundo,0,0);
+    objContexto.drawImage(imgHeroi, xHeroiR, yHeroiR);
+    objContexto.drawImage(imgMonstro, xMonstroR, yMonstroR);
+}
 function Iniciar()
 {
     objCanvas = document.getElementById("meuCanvas");
     objContexto = objCanvas.getContext("2d");
-
     objContexto.drawImage(imgFundo,0,0);
 
     AtualizaTela();
@@ -49,46 +62,80 @@ function AtualizaTela()
 
     //taxa de incremento
     var taxa = 10;
-
+    
     function leDoTeclado(evento){
         /*sabemos que é através do evento.keyCode
         que temos acesso ao cód da tecla
         pressionada.*/
         var teclaPressionada = evento.keyCode;
 
-        if (teclaPressionada == esquerda) {  
+        if (teclaPressionada == esquerda)
+        {  
+            if(xHeroi > 32)
+            {
                 MovimentoDoHeroi(2)
+            }
+            else
+            {
+                alert("O HERÓI MORREU:( TENTE NOVAMENTE!")
+                Resetar();    
+            }
         } 
-        else if (teclaPressionada == cima) {
-            MovimentoDoHeroi(3)
-          
-        } else if (teclaPressionada == direita) {
-            
-                MovimentoDoHeroi(0)
-        } else if (teclaPressionada == baixo) {
-            MovimentoDoHeroi(1)
-        }
-        else if(teclaPressionada == w)
+        else if (teclaPressionada == cima) 
         {
-            MovimentoDoMonstro(3)
-        }
-        else if (teclaPressionada == a) {
-            MovimentoDoMonstro(2)
+            if(yHeroi > 32 )
+            {
+                MovimentoDoHeroi(3)
+            }
+            else
+            {
+                alert("O HERÓI MORREU :( TENTE NOVAMENTE!")
+                Resetar();    
+            }
         } 
-        else if (teclaPressionada == d) {
-            MovimentoDoMonstro(0)
-
-        } else if (teclaPressionada == s) {
-            MovimentoDoMonstro(1)
+        else if (teclaPressionada == direita) 
+        {
+            if(xHeroi < 512 - 64)
+            {
+                MovimentoDoHeroi(0)
+            }
+            else
+            {
+                alert("O HERÓI MORREU :( TENTE NOVAMENTE!")
+                Resetar();
+            }
         }
+        else if (teclaPressionada == baixo)
+        {
+            if(yHeroi < 480 - 64)
+            {
+                MovimentoDoHeroi(1)
+            }
+            else
+            {
+                alert("O HERÓI MORREU :( TENTE NOVAMENTE!")
+                Resetar();
+            }
+        }
+        if(yMonstro > yHeroi - 64 || xMonstro > xHeroi - 64)
+        {
+                if(yMonstro < 480 - 64 && xMonstro < 512 - 64 && xMonstro > 32 && yMonstro > 32)
+                    MovimentoDoMonstro(30)
+                else{
+                    alert("O MONSTRO MORREU :) TENTE NOVAMENTE!")
+                    Resetar();
+                }
+        }
+        MovimentoDoMonstro(10)
     }
     document.onkeydown = leDoTeclado;
 
 //
-function MovimentoDoHeroi (mov)
+function MovimentoDoHeroi (movH)
 {
     // sorteia uma direção para a qual o heroi vai se dirigir
-    switch (mov)
+    
+    switch (movH)
     {
         case 0:xHeroi=(xHeroi + 10); break;
         case 1:yHeroi=(yHeroi + 10); break;
@@ -100,12 +147,93 @@ function MovimentoDoHeroi (mov)
 
 function MovimentoDoMonstro (mov)
 {
-    switch (mov)
+    var direcao = parseInt((Math.random()*mov) % 4);
+    switch (direcao)
     {
-        case 0:xMonstro=(xMonstro + 10); break;
-        case 1:yMonstro=(yMonstro + 10); break;
-        case 2:xMonstro=(xMonstro - 10); break;
-        case 3:yMonstro=(yMonstro - 10); break;
+        case 0:xMonstro=(xMonstro + mov); break;
+        case 1:yMonstro=(yMonstro + mov); break;
+        case 2:xMonstro=(xMonstro - mov); break;
+        case 3:yMonstro=(yMonstro - mov); break;
     }
     AtualizaTela();
-} 
+}
+
+const minutesEl      = document.querySelector("#minutes")
+const secondsEl      = document.querySelector("#seconds")
+const millisecondsEl = document.querySelector("#milliseconds")
+const startBtn       = document.querySelector("#startBtn")
+const pauseBtn       = document.querySelector("#pauseBtn")
+const resumeBtn      = document.querySelector("#resumeBtn")
+const resetBtn       = document.querySelector("#resetBtn")
+
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
+
+startBtn.addEventListener("click", ComecarTempo)
+pauseBtn.addEventListener("click", PararTempo)
+resumeBtn.addEventListener("click", ContinuarTempo)
+resetBtn.addEventListener("click", ResetarTempo)
+
+function ComecarTempo() {
+	interval = setInterval(() => {
+
+		if(!isPaused) {
+			milliseconds += 10;
+
+			if(milliseconds === 1000) {
+				seconds++;
+				milliseconds = 0;
+			}
+
+			if(seconds === 60) {
+				minutes++;
+				seconds = 0;
+			}
+
+			minutesEl.textContent = FormatarTempo(minutes);
+			secondsEl.textContent = FormatarTempo(seconds);
+			millisecondsEl.textContent =  FormatarMilissegundos(milliseconds);
+		}
+
+	}, 10)
+
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+}
+
+function PararTempo() {
+    isPaused = true;
+    pauseBtn.style.display  = "none";
+    resumeBtn.style.display = "block";
+}
+
+function ContinuarTempo() {
+    isPaused = false;
+    pauseBtn.style.display  = "block";
+    resumeBtn.style.display = "none";
+}
+
+function ResetarTempo() {
+    clearInterval(interval);
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+
+    minutesEl.textContent = "00"
+	secondsEl.textContent = "00"
+	millisecondsEl.textContent = "000"
+
+    startBtn.style.display   = "block";
+    pauseBtn.style.display   = "none";
+    resumeBtn.style.display  = "none";
+}
+
+function FormatarTempo(time) {
+	return time < 10 ? `0$(time)` : time;
+}
+
+function FormatarMilissegundos(time) {
+	return time < 100 ? `$(time)`.padStart(3, "0") : time;
+}
